4473547b0af6bcdc7750c919acdb809889605a28
Christian C <caneovilches46@gmail.com>
2024-08-31 23:21:06 -0400

    Fin seccion DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { DbzModule } from './dbz/dbz.module';
+import { MainPagePipe } from './pages/main-page.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainPagePipe,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/main-page.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MainPagePipe } from './main-page.pipe';
+
+describe('MainPagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new MainPagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
